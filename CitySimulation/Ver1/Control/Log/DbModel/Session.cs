aca7bc57bda5a9e18df97eb909b72b3a3b8b08de
dark1103 <milaev.artemiy@yandex.ru>
2021-08-08 13:34:41 +0300

    fix

---
+++ CitySimulation/Ver1/Control/Log/DbModel/Session.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CitySimulation.Control.Log.DbModel
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
