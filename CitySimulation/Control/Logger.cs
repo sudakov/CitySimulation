c826fa34b50d6bcfba91f320310398302320b330
dark1103 <milaev.artemiy@yandex.ru>
2021-08-08 13:34:53 +0300

    fix

---
+++ CitySimulation/Control/Logger.cs
@@ -0,0 +1,18 @@
+﻿using CitySimulation.Entity;
+using CitySimulation.Tools;
+
+namespace CitySimulation.Control
+{
+    public abstract class Logger : Module
+    {
+        public int SessionId { get; protected set; } = -1;
+
+        public abstract void LogPersonInFacilityTime(LogCityTime start, LogCityTime end, Facility facility, Person person);
+
+        public abstract int? Start();
+
+        public abstract void Stop();
+
+        public abstract void LogVisit(Service service);
+    }
+}
